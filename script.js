f58dbf3d63f0844fa30e5f28b6018ebd56f7336b
pesu <peter.sukhenko@gmail.com>
2018-10-25 11:50:15 +0300

    Small refactoring + resizing bug fixed

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ function hasGetUserMedia() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
-const constraints = {
-  video: true
-};
 
 const video = document.querySelector('#video');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 function initVideo() {
+  const constraints = {
+    video: true
+  };
+
   navigator.mediaDevices.getUserMedia(constraints).then(stream => {
     video.srcObject = stream;
   });
@@ -37,12 +38,18 @@ function resizeCanvasTo(element) {
   canvas.height = element.offsetHeight;
 }
 
-
-const MARKER_SIZE = 6;
-
-const dim = canvas.getBoundingClientRect();
 canvas.onclick = function (event) {
-  tagTracker.addMarker(event.clientX - dim.left, event.clientY - dim.top);
+  const dim = canvas.getBoundingClientRect();
+  tagTracker.addTag(event.clientX - dim.left, event.clientY - dim.top);
+}
+
+function drawMarker(marker) {
+  const MARKER_SIZE = 6;
+  const color = 'red';
+
+  context.fillStyle = color;
+  context.strokeStyle = color;
+  context.fillRect(marker.x - MARKER_SIZE / 2, marker.y - MARKER_SIZE / 2, MARKER_SIZE, MARKER_SIZE);
 }
 
 
@@ -54,35 +61,22 @@ const TagTracker = function() {
 tracking.inherits(TagTracker, tracking.Tracker);
 
 TagTracker.prototype.track = function(pixels, width, height) {
-  const results = [];
+  const results = this.tags;
 
   this.emit('track', {
     data: results
   });
 }
 
-TagTracker.prototype.addMarker = function (x, y) {
+TagTracker.prototype.addTag = function (x, y) {
   this.tags.push({ x: x, y: y });
 }
-
-TagTracker.prototype.drawMarkers = function () {
-  this.tags.forEach(marker => { 
-    const color = 'red';
-    context.fillStyle = color;
-    context.strokeStyle = color;
-    context.fillRect(marker.x - MARKER_SIZE / 2, marker.y - MARKER_SIZE / 2, MARKER_SIZE, MARKER_SIZE);
-  });
-}
-
 
 const tagTracker = new TagTracker();
 
 tagTracker.on('track', function (event) {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  tagTracker.drawMarkers();
-  event.data.forEach(rect => {
-  
-  });
+  event.data.forEach(drawMarker);
 });
 
 const tracker = tracking.track('#video', tagTracker);
